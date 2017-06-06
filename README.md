276f576304377fab6346d574deed2cde7092d6e1
jayd <jayd@jayd-OptiPlex-990>
2017-06-06 20:47:54 +0800

    ready to run a fetch data server.

--- README.md
+++ README.md
@@ -35,26 +35,8 @@ if encountered 'ActionView::Template::Error (Permission denied @ utime_internal 
 rm -rf tmp/cache
 ```
 
-
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+To start fetch current cost data:
+```sh
+cd script
+nohup python -u fetchFromPachube.py > log-fetchfrompachube.txt &
+```
--- scripts/fetchFromPachube.py
+++ scripts/fetchFromPachube.py
@@ -1,17 +1,18 @@
+import os
+import sys
 import sqlite3
 import datetime
 import random
 import time
 import urllib, urllib2
 import json
+import traceback
 
-url = 'http://api.pachube.com/v2/feeds/458165108.json?key=FkKmAX3afWITYmCEVcVG6mBopu5fdCoXZNbXC5pE8lppIEDR'
+#logfile = open('log-fetchfrompachube.txt', 'w')
 
-conn = sqlite3.connect('../db/development.sqlite3')
-c = conn.cursor()
-
-last = datetime.datetime(2000, 1, 6, 13, 1, 1)
-while 1:
+def fetchOnePerson(feedId, apiKey, last, conn):
+    c = conn.cursor()
+    url = 'http://api.pachube.com/v2/feeds/' + feedId + '.json?key=' + apiKey
     # GET 
     req = urllib2.Request(url)
     # Response
@@ -29,11 +30,51 @@ while 1:
     second = int(timestamp[17:19])
 
     d = datetime.datetime(year, month, day, hour, minute, second)
-    if d != last:
-        print d, current_value
-        c.execute("INSERT INTO usages (feedId, timestamp, power, created_at, updated_at) VALUES ('458165108', '" + d.strftime('%Y%m%d %H:%M:%S') + "', " + current_value + ", '20170406 12:45:32', '20170406 12:45:32')")
-        conn.commit()
-    last = d
-    time.sleep(60)
+    now = datetime.datetime.now()
 
+    if d != last:
+        print feedId, d, current_value
+        #logfile.write(feedId + " " + d.strftime('%Y%m%d %H:%M:%S') + " " + str(current_value) + "\n")
+        c.execute("INSERT INTO usages (feedId, timestamp, power, created_at, updated_at) VALUES ('{0}', '{1}', {2}, '{3}', '{4}')".format(feedId, d.strftime('%Y%m%d %H:%M:%S'), current_value, now.strftime('%Y%m%d %H:%M:%S'), now.strftime('%Y%m%d %H:%M:%S')))
+    last = d
+    return last
+    
+def readUsers():
+    users = []
+    f = open('users.csv')
+    lines = f.readlines()
+    for line in lines[1:]:
+        items = line.strip().split(',')
+        users.append({'feedId':items[1], 'apiKey':items[2]})
+    f.close()
+    return users
+
+
+if __name__=="__main__":
+    users = readUsers()
+    # record the last edit time of file 'users.csv'
+    lasttimefilechange = os.path.getmtime('users.csv')
+    conn = sqlite3.connect('../db/production.sqlite3')
+    # the last time of data changed for a feedId
+    last = {}
+    #for each in users:
+    #    last[each['feedId'] = datetime.datetime(2000, 1, 6, 13, 1, 1)
+    while 1:
+        # Check if the 'users.csv' has changed, if changed update the users array.
+        if os.path.getmtime('users.csv') != lasttimefilechange:
+            lasttimefilechange = os.path.getmtime('users.csv')
+            users = readUsers()
+        # For each user, check whether there is update
+        for each in users:
+            try:
+                if each['feedId'] not in last:
+                    last[each['feedId']] = datetime.datetime(2000, 1, 6, 13, 1, 1)
+                last[each['feedId']] = fetchOnePerson(each['feedId'], each['apiKey'], last[each['feedId']], conn)
+            except:
+                print "Unexpected error when fetch feedId = " + each['feedId'] + ":", sys.exc_info()[0]
+                #logfile.write("Unexpected error when fetch feedId = " + each['feedId'] + ": " + traceback.format_exc())
+
+        conn.commit()
+        time.sleep(10)
+    
 conn.close()
